509a9d57709814e671d4e18ed861b0c60c34f16f
AleksMor039 <aleks.mor045@gmail.com>
2024-12-23 17:46:16 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,16 +6,16 @@ from app.routers import user
 from app.backend.db import engine, Base
 
 # создали сущность FastAPI
-app = FastAPI()
+app1 = FastAPI()
 
 
-@app.get("/")  # созд.1 маршрут "/"
+@app1.get("/")  # созд.1 маршрут "/"
 async def welcome() -> dict:  # возвр.словарь
     return {"message": "Welcome to Taskmanager"}
 
 # подкл. объектов APIRouter к созд.FastAPI
-app.include_router(task.router)
-app.include_router(user.router)
+app1.include_router(task.router)
+app1.include_router(user.router)
 
 # if __name__ == 'main':
 #     uvicorn.run('main:app', host='127.0.0.1', port=8000, reload=True)
